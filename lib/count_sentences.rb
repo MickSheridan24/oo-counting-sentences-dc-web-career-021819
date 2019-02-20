68a80a174badc65ab6ad7956ab6e45b36218a31e
Mickey <micksheridan.24@gmail.com>
2019-02-20 16:33:13 -0500

    submit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with? (".")
   end
 
   def question?
-
+    return self.end_with? ("?")
   end
 
   def exclamation?
-
+    return self.end_with? ("!")
   end
 
   def count_sentences
-
+    count = 0
+    e = 0
+    
+    while e < self.length
+      if (self[e] == "." || self[e] == "?" || self[e] == "!")
+        count+=1
+        e+=2
+      end
+      e+=1
+    end
+    count
   end
 end
